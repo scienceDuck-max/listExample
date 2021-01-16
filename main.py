ce888ffabbdd3571b449e41f9977257ddb20e3c3
printlnsd <kellypro03@gmail.com>
2021-01-16 01:57:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+testList = [1,2,3]
+print(testList)
+
+
+#append
+testList.append(4)
+print(testList)
+
+#insert
+testList.insert(1,1.5)
+print(testList)
+
+#delete
+del testList[2]
+print(testList)
+
+#pop
+popValue = testList.pop()
+print(popValue)
+print(testList)
